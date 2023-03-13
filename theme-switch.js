9bee1c4dc076328447b3963b4ac7abc1bec4725c
ickk <git@ickk.io>
2023-03-14 03:52:28 +1100

    fixup

--- theme-switch.js
+++ theme-switch.js
@@ -4,12 +4,12 @@ const html = document.querySelector('html');
 window.addEventListener('DOMContentLoaded', function() {
   // Add the theme-switch element.
   document.body.insertAdjacentHTML('afterbegin',
-    '<theme-switch>' +
+    '<div class=theme-switch>' +
       '<label for=theme-light>light-theme</label>' +
       '<input type=radio id=theme-light name=theme value=light>' +
       '<label for=theme-dark>dark-theme</label>' +
       '<input type=radio id=theme-dark name=theme value=dark>' +
-    '</theme-switch>'
+    '</div>'
   );
   // Look for theme setting in local storage or fallback to OS theme.
   if (localStorage.getItem('theme'))
@@ -18,11 +18,11 @@ window.addEventListener('DOMContentLoaded', function() {
     html.dataset.theme = 'dark'
   // Select corresponding radio input.
   if (html.dataset.theme === 'dark')
-    document.querySelector('theme-switch input[value=dark]').checked = true;
+    document.querySelector('.theme-switch input[value=dark]').checked = true;
   else
-    document.querySelector('theme-switch input[value=light]').checked = true;
+    document.querySelector('.theme-switch input[value=light]').checked = true;
   // Bind event listeners for explicit theme selection.
-  document.querySelectorAll('theme-switch input[type=radio]').forEach(
+  document.querySelectorAll('.theme-switch input[type=radio]').forEach(
     input => input.addEventListener('change', (event) => {
       html.dataset.theme = event.target.value;
       localStorage.setItem('theme', html.dataset.theme);
